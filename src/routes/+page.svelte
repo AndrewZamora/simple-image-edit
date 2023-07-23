f2a8b8df7de2c87035bb7296d152d3922720f251
AndrewZamora <andrewmichaelzamora@gmail.com>
2023-07-23 22:29:00 +0900

    wip: added fabric

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// @ts-nocheck
+	import { fabric } from 'fabric';
+	import { onMount } from 'svelte';
+	/**
+	 * @type {HTMLCanvasElement}
+	 */
+	let id;
+
+	onMount(() => {
+		const canvas = new fabric.Canvas(id);
+		const rect = new fabric.Rect({
+			top: 100,
+			left: 100,
+			width: 60,
+			height: 70,
+			fill: 'red'
+		});
+		canvas.add(rect);
+	});
+</script>
+
+<div class="index">
+	<canvas bind:this={id} width="300" height="300" />
+</div>
