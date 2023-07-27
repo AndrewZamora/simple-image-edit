5934c783be734b54d354f37354e950001356c6ee
AndrewZamora <andrewmichaelzamora@gmail.com>
2023-07-27 23:44:07 +0900

    wip: got a textbox to show on the canvas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,44 @@
 	 * @type {HTMLCanvasElement}
 	 */
 	let id;
-
 	onMount(() => {
-		const canvas = new fabric.Canvas(id);
+		const canvas = new fabric.Canvas(id, { height: 500, width: 500 });
 		const rect = new fabric.Rect({
-			top: 100,
+			top: 0,
 			left: 100,
 			width: 60,
 			height: 70,
 			fill: 'red'
 		});
+		const textbox = new fabric.Textbox("this is a test", {
+			top: 50,
+			left: 10,
+		})
+		canvas.add(textbox)
 		canvas.add(rect);
+		canvas.on('mouse:over', (event) => {
+			console.log(event);
+		});
 	});
+
+	function setBackgroundImage() {}
 </script>
 
 <div class="index">
-	<canvas bind:this={id} width="300" height="300" />
+	<main class="container">
+		<canvas class="canvas" bind:this={id} width="300" height="300" />
+	</main>
 </div>
+
+<style>
+	.container {
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		outline: orangered dashed 1px;
+	}
+	.canvas {
+		outline: green dashed 1px;
+	}
+</style>
